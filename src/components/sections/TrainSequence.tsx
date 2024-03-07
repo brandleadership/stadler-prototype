a0dd901b3f5059eb1cdd71ab1ba39dd1a8954e7e
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-03-07 09:46:41 +0100

    fix: requestAnimationFrame function add

--- src/components/sections/TrainSequence.tsx
+++ src/components/sections/TrainSequence.tsx
@@ -26,7 +26,7 @@ const handleDrawCanvas = (
   const ratio = Math.max(widthRatio, heightRatio);
   const centerX = (canvas.width - img.width * ratio) / 2;
   const centerY = (canvas.height - img.height * ratio) / 2;
-  console.log(widthRatio, heightRatio);
+  // console.log(widthRatio, heightRatio);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(
     img,
@@ -56,6 +56,7 @@ const ImageSequence = ({ category }: { category: Number }) => {
 
   // const containerRef = useRef<HTMLElement>(null);
   const [prevCategoryNumber, categoryNumberChange] = useState(category);
+  console.log("prevCategoryNumber ",category, prevCategoryNumber)
   const [animationProgress, animationProgressChange] = useState(false);
   const [progress, canvasRef, renderImage] = useScrollImageSequenceFramerCanvas(
     {
@@ -64,7 +65,90 @@ const ImageSequence = ({ category }: { category: Number }) => {
       keyframes: keyframes,
     }
   );
+  let start: any, previousTimeStamp: any;
+let done = false; 
+let x = Number(prevCategoryNumber) / 3;
+let c = Number(category) / 3;
+  const changeCarouselPositions = (timeStamp) => {
+    const singleSlideProgress = 1 / 100;
+    let count;
+    if (start === undefined) {
+      start = Number(prevCategoryNumber) / 3;
+    }
+    if (c > x) {
+      const categoryDifference = Number(category) - Number(prevCategoryNumber);
+      const progressDifference = categoryDifference / 3 + Number(prevCategoryNumber) / 3;
+      count = start + singleSlideProgress; 
+      renderImage(count)
+      start = start + singleSlideProgress;
+      if (start >= progressDifference) {
+        done = true;
+        categoryNumberChange(category)
+      } else {
+        if (!done) {
+          window.requestAnimationFrame(changeCarouselPositions);
+        }
+      }
+      console.log("elapsed ",prevCategoryNumber, category, progressDifference, count, start)
+    }
 
+    
+    if (c < x) {
+      const categoryDifference = Number(prevCategoryNumber) - Number(category);
+      const progressDifference = Number(prevCategoryNumber) / 3 - categoryDifference / 3 ;
+      count = start - singleSlideProgress; 
+      renderImage(count)
+      start = start - singleSlideProgress;
+      if (start <= progressDifference) {
+        done = true;
+        categoryNumberChange(category)
+      } else {
+        if (!done) {
+          window.requestAnimationFrame(changeCarouselPositions);
+        }
+      }
+      console.log("elapsed ",prevCategoryNumber, category, progressDifference, count, start)
+    }
+    // const elapsed = timeStamp - start;
+  
+   
+      // Math.min() is used here to make sure the element stops at exactly 200px
+      // console.log("count", count)
+     
+     
+      
+   
+  
+    // if (elapsed < 4000) {
+    //   // Stop the animation after 2 seconds
+    //   previousTimeStamp = timeStamp;
+      
+    // }
+    // let x = Number(prevCategoryNumber) / 3;
+    // let c = Number(category) / 3;
+
+    // let timeDifference = c - x;
+    // const time = 500
+    // var i = 0
+    
+    // let slideDifference = (timeDifference / 500) * 15;
+    
+   
+
+    // if (slideDifference < 1) {
+     
+    //   // Stop the animation after 2 seconds
+      
+        
+    // }
+    // console.log("wtf", prevCategoryNumber, i);
+    // if (i < 200) {
+    //   // Stop the animation after 2 seconds
+    //   i = i + slideDifference
+    //   renderImage(i)
+    //   window.requestAnimationFrame(changeCarouselPositions);
+    // }
+  }
   useEffect(() => {
     // console.log(
     //   "counter updated",
@@ -73,89 +157,96 @@ const ImageSequence = ({ category }: { category: Number }) => {
     //   prevCategoryNumber
     // );
     if (!animationProgress) {
-      // console.log("first if 1");
+      console.log("x c", x, c)
+      if (x != c) {
+      window.requestAnimationFrame(changeCarouselPositions);
+    }
+      
+      
 
-      let x = Number(prevCategoryNumber) / 3;
-      let c = Number(category) / 3;
+//       let x = Number(prevCategoryNumber) / 3;
+//       let c = Number(category) / 3;
+// console.log(x, c)
+//       if (x < c) {
+//         animationProgressChange(true);
+//         const t = setInterval(function() {
+//           x += 0.015;
+//           if (x <= c) {
+//             renderImage(x);
+//           } else {
+//             // console.log("clear interval 1");
+//             clearInterval(t);
+//             animationProgressChange(false);
+//             categoryNumberChange(category);
+//             // console.log("check c", c, category);
+//           }
+//         }, 16.7);
+//       }
+//       if (x > c) {
+//         // console.log("second if", x, c);
+//         animationProgressChange(true);
+//         const t = setInterval(function() {
+//           x -= 0.015;
+//           if (x >= c) {
+//             renderImage(x);
+//           } else {
+//             // console.log("clear interval 2");
+//             clearInterval(t);
+//             animationProgressChange(false);
 
-      if (x < c) {
-        animationProgressChange(true);
-        const t = setInterval(function() {
-          x += 0.015;
-          if (x <= c) {
-            renderImage(x);
-          } else {
-            // console.log("clear interval 1");
-            clearInterval(t);
-            animationProgressChange(false);
-            categoryNumberChange(category);
-            console.log("check c", c, category);
-          }
-        }, 16.7);
-      }
-      if (x > c) {
-        // console.log("second if", x, c);
-        animationProgressChange(true);
-        const t = setInterval(function() {
-          x -= 0.015;
-          if (x >= c) {
-            renderImage(x);
-          } else {
-            // console.log("clear interval 2");
-            clearInterval(t);
-            animationProgressChange(false);
-
-            categoryNumberChange(category);
-            console.log("check c", c, category);
-          }
-        }, 16.7);
-      }
+//             categoryNumberChange(category);
+//             // console.log("check c", c, category);
+//           }
+//         }, 16.7);
+//       }
     }
   }, [category]);
 
-  const handleClick = (curState = 0, nextState = 1) => {
-    // console.log("selectedCategory", selectedCategory);
-    if (!animationProgress) {
-      animationProgressChange(true);
-      categoryNumberChange(nextState);
 
-      let x = curState;
 
-      if (curState < nextState) {
-        const t = setInterval(function() {
-          x += 0.015;
-          if (x <= nextState) {
-            renderImage(x);
-          } else {
-            console.log("clear interval 1");
-            clearInterval(t);
-            animationProgressChange(false);
-          }
-        }, 16.7);
-      }
-      if (curState > nextState) {
-        const t = setInterval(function() {
-          x -= 0.015;
-          if (x >= nextState) {
-            renderImage(x);
-          } else {
-            console.log("clear interval 2");
-            clearInterval(t);
-            animationProgressChange(false);
-          }
-        }, 16.7);
-      }
-    }
-    // x.set(0.5);
-    // useMotionValueEvent(0.5, "change", renderImage);
-    // categoryNumberChange(0.5);
-    // renderImage(0.5);
-    console.log(
-      "categoryNumber",
-      prevCategoryNumber,
-      typeof prevCategoryNumber
-    );
-  };
+  // const handleClick = (curState = 0, nextState = 1) => {
+  //   // console.log("selectedCategory", selectedCategory);
+  //   if (!animationProgress) {
+  //     animationProgressChange(true);
+  //     categoryNumberChange(nextState);
+
+  //     let x = curState;
+
+  //     if (curState < nextState) {
+  //       const t = setInterval(function() {
+  //         x += 0.015;
+  //         if (x <= nextState) {
+  //           renderImage(x);
+  //         } else {
+  //           console.log("clear interval 1");
+  //           clearInterval(t);
+  //           animationProgressChange(false);
+  //         }
+  //       }, 16.7);
+  //     }
+  //     if (curState > nextState) {
+  //       const t = setInterval(function() {
+  //         x -= 0.015;
+  //         if (x >= nextState) {
+  //           renderImage(x);
+  //         } else {
+  //           console.log("clear interval 2");
+  //           clearInterval(t);
+  //           animationProgressChange(false);
+  //         }
+  //       }, 16.7);
+  //     }
+  //   }
+  //   // x.set(0.5);
+  //   // useMotionValueEvent(0.5, "change", renderImage);
+  //   // categoryNumberChange(0.5);
+  //   // renderImage(0.5);
+  //   console.log(
+  //     "categoryNumber",
+  //     prevCategoryNumber,
+  //     typeof prevCategoryNumber
+  //   );
+  // };
 
   return (
     <section>
